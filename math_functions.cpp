19d642ba4a6ee12f2d0c94f200710b7c8c3cbb5f
Pranit Bauva <pranit.bauva@gmail.com>
2015-12-01 16:39:25 +0530

    Add a program for showing the mathematical functions available in the library

---
+++ math_functions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int d = 10;
+
+    cout << "The value of the variable d is : " << d << endl;
+    cout << "sin(d) : " << sin(d) << endl;
+    cout << "cos(d) : " << cos(d) << endl;
+    cout << "tan(d) : " << tan(d) << endl;
+    cout << "log(d) : " << log(d) << endl;
+    cout << "pow(d,2) : " << pow(d,2) <<endl;
+    cout << "sqrt(d) : " << sqrt(d) << endl;
+}
