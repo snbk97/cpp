6f935e1d7587781a7afc59d95b0e1a9b4d1293be
Pranit Bauva <pranit.bauva@gmail.com>
2015-12-04 15:28:50 +0530

    Declare functions outside the class using '::' operator

--- class_member_functions.cpp
+++ class_member_functions.cpp
@@ -8,16 +8,30 @@ class Box{
         double b;
         double h;
 
-        double getVolume(void){
-            return l*b*h;
-        }
+        double getVolume(void);
+        void setLength(double len);
+        void setBreadth(double breadth);
+        void setHeight(double height);
 };
+
+double Box::getVolume(void){
+    return l*b*h;
+}
+void Box::setLength(double len){
+    l = len;
+}
+void Box::setBreadth(double breadth){
+    b = breadth;
+}
+void Box::setHeight(double height){
+    h = height;
+}
 
 int main(){
     Box box;
-    box.l = 10;
-    box.b = 8;
-    box.h = 5;
+    box.setLength(10);
+    box.setBreadth(8);
+    box.setHeight(5);
 
     double vol = box.getVolume();
     cout << "The volume of the box is : " << vol << endl;
